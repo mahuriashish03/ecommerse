a681c100852597f2ab8bc817ec16710a08591356
mahuriashish03 <mahuriashish03@gmail.com>
2025-10-08 17:20:10 +0200

    Product Service: Bug fix for skuCode to store mongo document.

--- product-service/src/main/java/com/ecommerse/microservices/product/dto/ProductResponse.java
+++ product-service/src/main/java/com/ecommerse/microservices/product/dto/ProductResponse.java
@@ -2,4 +2,4 @@ package com.ecommerse.microservices.product.dto;
 
 import java.math.BigDecimal;
 
-public record ProductResponse(String id, String name, String description, BigDecimal price ) { }
+public record ProductResponse(String id, String skuCode, String name, String description, BigDecimal price ) { }
